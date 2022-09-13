86b93567e7a5ebac35c4b52d69d172cd4e8440d3
Dschogo <36862419+Dschogo@users.noreply.github.com>
2022-09-13 17:11:24 +0200

    fucking works

--- Assets/WayspotAnchorDataUtility.cs
+++ Assets/WayspotAnchorDataUtility.cs
@@ -9,60 +9,76 @@ using UnityEngine;
 
 namespace WayspotAnchors
 {
-  public static class WayspotAnchorDataUtility
-  {
-    private const string DataKey = "wayspot_anchor_payloads";
-    
-    public static void SaveLocalPayloads(WayspotAnchorPayload[] wayspotAnchorPayloads)
+    public static class WayspotAnchorDataUtility
     {
-      var wayspotAnchorsData = new WayspotAnchorsData();
-      wayspotAnchorsData.Payloads = wayspotAnchorPayloads.Select(a => a.Serialize()).ToArray();
-      string wayspotAnchorsJson = JsonUtility.ToJson(wayspotAnchorsData);
-      PlayerPrefs.SetString(DataKey, wayspotAnchorsJson);
-    }
+        private const string DataKey = "wayspot_anchor_payloads";
 
-    public static WayspotAnchorPayload[] LoadLocalPayloads()
-    {
-      if (PlayerPrefs.HasKey(DataKey))
-      {
-        var payloads = new List<WayspotAnchorPayload>();
-        // var json = PlayerPrefs.GetString(DataKey);
-        // var wayspotAnchorsData = JsonUtility.FromJson<WayspotAnchorsData>(json);
-        var payloads_serialized = new string[0];
-        if(Stateholder.pois[Stateholder.curr_poi].following != null)
+        public static void SaveLocalPayloads(WayspotAnchorPayload[] wayspotAnchorPayloads)
         {
-          payloads_serialized = Stateholder.pois[Stateholder.curr_poi].following[Stateholder.curr_schnitzel_counter].anchors;
+            var wayspotAnchorsData = new WayspotAnchorsData();
+            wayspotAnchorsData.Payloads = wayspotAnchorPayloads.Select(a => a.Serialize()).ToArray();
+            string wayspotAnchorsJson = JsonUtility.ToJson(wayspotAnchorsData);
+            PlayerPrefs.SetString(DataKey, wayspotAnchorsJson);
         }
-        else {
-          payloads_serialized = Stateholder.pois[Stateholder.curr_poi].anchors;
-        }
-        foreach (var wayspotAnchorPayload in payloads_serialized)
+
+        public static WayspotAnchorPayload[] LoadLocalPayloads()
         {
-          var payload = WayspotAnchorPayload.Deserialize(wayspotAnchorPayload);
-          payloads.Add(payload);
+            //if (PlayerPrefs.HasKey(DataKey))
+            //{
+            var payloads = new List<WayspotAnchorPayload>();
+            // var json = PlayerPrefs.GetString(DataKey);
+            // var wayspotAnchorsData = JsonUtility.FromJson<WayspotAnchorsData>(json);
+            var payloads_serialized = new string[0];
+
+            if (Stateholder.pois[Stateholder.curr_poi].following != null)
+            {
+                if (Stateholder.pois[Stateholder.curr_poi].following[Stateholder.curr_schnitzel_counter].anchors != null)
+                {
+                    payloads_serialized = Stateholder.pois[Stateholder.curr_poi].following[Stateholder.curr_schnitzel_counter].anchors;
+                }
+                else
+                {
+                    return Array.Empty<WayspotAnchorPayload>();
+                }
+            }
+            else
+            {
+                if (Stateholder.pois[Stateholder.curr_poi].anchors != null)
+                {
+                    payloads_serialized = Stateholder.pois[Stateholder.curr_poi].anchors;
+                }
+                else
+                {
+                    return Array.Empty<WayspotAnchorPayload>();
+                }
+            }
+            foreach (var wayspotAnchorPayload in payloads_serialized)
+            {
+                var payload = WayspotAnchorPayload.Deserialize(wayspotAnchorPayload);
+                payloads.Add(payload);
+            }
+
+            return payloads.ToArray();
+            //}
+            //else
+            //{
+            //  return Array.Empty<WayspotAnchorPayload>();
+            //}
         }
 
-        return payloads.ToArray();
-      }
-      else
-      {
-        return Array.Empty<WayspotAnchorPayload>();
-      }
-    }
+        public static void ClearLocalPayloads()
+        {
+            if (PlayerPrefs.HasKey(DataKey))
+            {
+                PlayerPrefs.DeleteKey(DataKey);
+            }
+        }
 
-    public static void ClearLocalPayloads()
-    {
-      if (PlayerPrefs.HasKey(DataKey))
-      {
-        PlayerPrefs.DeleteKey(DataKey); 
-      }
+        [Serializable]
+        private class WayspotAnchorsData
+        {
+            /// The payloads to save via JsonUtility
+            public string[] Payloads = Array.Empty<string>();
+        }
     }
-    
-    [Serializable]
-    private class WayspotAnchorsData
-    {
-      /// The payloads to save via JsonUtility
-      public string[] Payloads = Array.Empty<string>();
-    }
-  }
 }
